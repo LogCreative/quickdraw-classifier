a358bcf2ce19ef26a882a3102a9fba1f757146bc
tk <2774315388@qq.com>
2022-05-25 00:55:52 +0800

    add GetDataset func

--- load.py
+++ load.py
@@ -1,4 +1,12 @@
 import numpy as np
+import os
+import torch
+from torchvision import transforms
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+from PIL import Image
+import gc
+
 
 categories = [
     "bear",
@@ -28,7 +36,60 @@ categories = [
     "zebra",
 ]
 
-bear = np.load("dataset/sketchrnn_bear.npz", allow_pickle=True, encoding="latin1")
+categories_label_dict = dict(zip(categories, range(len(categories))))
+
+class MyDataset(Dataset):
+    def __init__(self, data, targets, transform=None):
+        self.data = data
+        self.targets = torch.LongTensor(targets)
+        self.transform = transform
+        
+    def __getitem__(self, index):
+        x = self.data[index]
+        y = self.targets[index]
+        
+        if self.transform:
+            x = Image.fromarray(self.data[index].astype(np.uint8))
+            x = self.transform(x)
+        
+        return x, y
+    
+    def __len__(self):
+        return len(self.data)
+
+def GetDataset(dataroot:str,transform=None):
+    filenames = next(os.walk(dataroot), (None, None, []))[2] 
+    filename = filenames[0]
+    category = filename[:-8]
+    npz_file = np.load(f'{dataroot}/{filename}',allow_pickle=True, encoding="latin1")
+    train_data_array = npz_file['train']
+    val_data_array = npz_file['train']
+    train_label_array = np.full((len(train_data_array),),categories_label_dict[category])
+    val_label_array = np.full((len(val_data_array),),categories_label_dict[category])
+    for filename in filenames[1:2]:
+        category = filename[:-8]
+        npz_file = np.load(f'{dataroot}/{filename}',allow_pickle=True, encoding="latin1")
+        train_data_array = np.concatenate((train_data_array, npz_file['train']), axis=0)
+        val_data_array = np.concatenate((val_data_array, npz_file['valid']), axis=0)
+        train_label_array = np.concatenate((train_label_array, np.full((len(npz_file['train']),),categories_label_dict[category])), axis=0)
+        val_label_array = np.concatenate((val_label_array, np.full((len(npz_file['valid']),),categories_label_dict[category])), axis=0)
+        gc.collect()
+    train_data_array = train_data_array.astype(np.float32)
+    val_data_array = val_data_array.astype(np.float32)
+    train_label_array = train_label_array
+    val_label_array = val_label_array
+    trainset,valset = MyDataset(train_data_array, train_label_array,transform), MyDataset(val_data_array, val_label_array,transform)
+    return trainset,valset
+        
+
+
+transform = transforms.Compose([transforms.Resize(256), transforms.ToTensor()])
+trainset,valset = GetDataset(dataroot='dataset',transform=transform)
+train_loader = DataLoader(trainset, batch_size=4, shuffle=True, num_workers=4)
+val_loader = DataLoader(valset, batch_size=4, num_workers=4)
+
+
+bear = np.load("dataset/bear_png.npz", allow_pickle=True, encoding="latin1")
 
 ## show the structure of bear
 # bear.files
