b1e6c8c89142cbc9aee9838ca0cbf81e2f3bd2f0
tk <2774315388@qq.com>
2022-05-26 11:25:28 +0800

    revise train_cnn and config_train_cnn

--- config_train_cnn.py
+++ config_train_cnn.py
@@ -1,12 +1,19 @@
 import torch
 
-class args:
+class Args:
     batch_size = 64
     val_batch_size = 64
     log_interval = 1000
-    epochs = 5
+    epochs = 10
     num_classes = 25
     dataroot = 'dataset/png'
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    model = 'sketchnet'
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    model = 'AlexNet'
     small_data = False
+    weight_decay = 1e-3
+    lr = 1e-3
+    def __init__(self) -> None:
+        pass
+    def __str__(self) -> str:
+        return f"batch_size: {self.batch_size}, val_batch_size: {self.val_batch_size}, log_interval: {self.log_interval}, epochs: {self.epochs}, num_classes: {self.num_classes}, dataroot: {self.dataroot}, device: {self.device}, model: {self.model}, small_data: {self.small_data}, weight_decay: {self.weight_decay}, lr: {self.lr}"
+args = Args()
--- train_cnn.py
+++ train_cnn.py
@@ -1,11 +1,13 @@
+from tabnanny import verbose
 import torch, os
 from torchvision.transforms import ToTensor, Compose, Resize, Grayscale
 from torch.utils import tensorboard as tb
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import DataLoader
 from config_train_cnn import args
 from image_loader import GetDataset
-from tqdm import tqdm
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+
 def main( args ):
     transform=Compose([Resize([225, 225]), Grayscale(), ToTensor()])
     trainset,valset = GetDataset(args.dataroot,transform,args.small_data)
@@ -16,21 +18,25 @@ def main( args ):
         from models.sketchnet import create_model
     elif args.model == 'AlexNet':
         from models.AlexNet import create_model
+    elif args.model == 'resnet18':
+        from models.resnet18 import create_model
     opt = {'num_classes':args.num_classes}
     model = create_model(opt)
     device = torch.device(args.device)
     
     crit = torch.nn.CrossEntropyLoss().to(device)
-    optim = torch.optim.Adam(model.parameters())
-
+    optim = torch.optim.AdamW(model.parameters(),lr=args.lr,weight_decay = args.weight_decay)
+    scheduler = ReduceLROnPlateau(optim, 'min', factor=0.3, patience=2,verbose=True)
     # Tensorboard stuff
-    # writer = tb.SummaryWriter('./logs')
     print("begin training")
     count = 0
+    best_accuracy = 0
     model.to(device)
     for e in range(args.epochs):
         model.train()
-        for i, (X, Y) in tqdm(enumerate(train_loader)):            
+        epoch_loss = 0.0
+        epoch_acc = 0
+        for i, (X, Y) in enumerate(train_loader):            
             X, Y = X.to(device), Y.to(device)
 
             optim.zero_grad()
@@ -39,29 +45,39 @@ def main( args ):
             loss = crit(output, Y)
             
             if i % args.log_interval == 0:
-                print(f'[Training] {i}/{e}/{args.epochs} -> Loss: {loss.item()}')
-                # writer.add_scalar('train-loss', loss.item(), count)
+                print(f'[Training] {i}/{e+1}/{args.epochs} -> Loss: {loss.item()}')
             
             loss.backward()
             optim.step()
 
             count += 1
+            epoch_loss += loss.item() * len(output)
+            _, predicted = torch.max(output, 1)
+            epoch_acc += (predicted == Y).sum().item()
+        
+        data_size = len(train_loader.dataset)
+        epoch_loss = epoch_loss / data_size
+        epoch_acc = float(epoch_acc) / data_size
+        print(f'Epoch {e + 1}/{args.epochs} | trainning | Loss: {epoch_loss:.4f} | Acc: {epoch_acc:.4f}')
+        scheduler.step(epoch_loss)
 
         correct, total = 0, 0
         model.eval()
-        for i, (X, Y) in enumerate(val_loader):
-            X, Y = X.to(device), Y.to(device)
-            output = model(X)
-            _, predicted = torch.max(output, 1)
-            total += Y.size(0)
-            correct += (predicted == Y).sum().item()
-            accuracy = (correct / total) * 100
-        
-        print(f'[validation] -/{e}/{args.epochs} -> Accuracy: {accuracy} %')
-        # writer.add_scalar('validation-accuracy', accuracy/100., e)
+        with torch.no_grad():
+            for i, (X, Y) in enumerate(val_loader):
+                X, Y = X.to(device), Y.to(device)
+                output = model(X)
+                _, predicted = torch.max(output, 1)                
+                correct += (predicted == Y).sum().item()
+        accuracy = (float(correct) / len(val_loader.dataset)) * 100
+        if accuracy > best_accuracy:
+            best_accuracy = accuracy
+            torch.save(model.state_dict(), f'best_{args.model}.pth')
+        print(f'[validation] -/{e+1}/{args.epochs} -> Accuracy: {accuracy} %')
 
 
 
 
 if __name__ == '__main__':
+    print(f"begin training and val with {args}") 
     main( args )
